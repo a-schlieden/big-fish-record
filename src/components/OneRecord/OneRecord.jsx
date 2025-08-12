98f4acd97399e38c409e15c539710b5308da3d19
Alexander <alex.schlieden@gmail.com>
2025-08-12 11:06:31 +0200

    code optimiiert

--- src/components/OneRecord/OneRecord.jsx
+++ src/components/OneRecord/OneRecord.jsx
@@ -1,38 +1,60 @@
-import * as Functions from '../../api/Functions';
-/* import vladImg from '../../images/vlad.jpg' */
+/* import * as Functions from '../../api/Functions'; */
+
 import style from './OneRecord.module.css';
 
 const OneRecord = ({ ourAllRecordsArray }) => {
     const ourOneRecordArray = ourAllRecordsArray;
-    /* console.log("ourOneRecordArray", ourOneRecordArray) */
-    /*     let some = ourAllRecordsArray;
-        console.log("ourAllRecordsArray", some)
-    
-        const nene = some.map(item => item);
-        console.log("item.users", nene)
-    
-        const newArrey = some.map(ArrItem => ArrItem.users).flatMap(user => user.alex.id)
-        console.log("newArrey", newArrey) */
+
+    const fischRercordWeight = (weight, length) => {
+        let fischWeightForPerson = " ";
+        if (weight > 0) {
+            fischWeightForPerson = <p>{weight / 1000} kg<br></br>{length} sm</p>
+        }
+        else {
+            fischWeightForPerson = <p> </p>;
+        }
+        return fischWeightForPerson;
+    }
+
+    const showOnerecord = (id, first, second) => {
+        return <div className="item" key={id}>
+            <div className={style.itemBody}>
+                <div className={style.itemFirst}>
+                    <img className={style.fisherImg} src={process.env.PUBLIC_URL + '/images/' + first.name + '.jpg'} alt="fisch-icon" />
+                    {fischRercordWeight(first.weight, first.length)}
+                </div>
+                <div className={style.itemSecond} style={{ height: `${(second.weight / first.weight) * 100}%` }}>
+                    <img className={style.fisherImg} src={process.env.PUBLIC_URL + '/images/' + second.name + '.jpg'} alt="fisch-icon" />
+                    {fischRercordWeight(second.weight, second.length)}
+                </div>
+            </div>
+        </div>;
+    }
+
 
     return (
         <>
             {ourOneRecordArray.map(ArrItem =>
                 <div key={ArrItem.id} className={style.oneRecordWrap}>
                     <div className={style.oneRecordHeaderWrap}>
-                        <img className={style.oneRecordHeaderImg} src={process.env.PUBLIC_URL + '/images/' + ArrItem.vlad.name + '.jpg'} alt="fisch-icon" />
-                        {/*    <img className={style.oneRecordHeaderImg} src="images/vlad.jpg" alt="fisch-icon" /> */}
-                        {/*    <img src={`${process.env.PUBLIC_URL}/images/vlad.jpg`} alt="foto" /> */}
-                        {/* `url(${'../images/' + FischTacklesArrItem + '.jpg'})` */}
-                        {/*    <span style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/images/' + ArrItem.vlad.name + '.jpg'})` }} className={style.oneRecordImge}>12</span> */}
+                        {/*  <img className={style.oneRecordHeaderImg} src={process.env.PUBLIC_URL + '/images/' + ArrItem.vlad.name + '.jpg'} alt="fisch-icon" /> */}
+                        {/* {funkRecord(ArrItem.vlad.name)} */}
                         <h4>{ArrItem.name}</h4>
                     </div>
                     {((ArrItem.alex.weight === 0 && ArrItem.vlad.weight === 0)) ?
-                        (<div style={{ minWidth: '300px' }}>No Info</div>) : (ArrItem.alex.weight > ArrItem.vlad.weight) ?
+                        (<div style={{ minWidth: '300px' }}>
+                            No Info
+                        </div>)
+                        :
+                        (ArrItem.alex.weight > ArrItem.vlad.weight)
+                            ?
                             (<div style={{ minWidth: '300px' }}>
-                                {Functions.oneRecordFn(ArrItem.id, ArrItem.alex, ArrItem.vlad)}
+                                {/* {Functions.oneRecordFn(ArrItem.id, ArrItem.alex, ArrItem.vlad)} */}
+                                {showOnerecord(ArrItem.id, ArrItem.alex, ArrItem.vlad)}
                             </div>)
-                            : (<div style={{ minWidth: '300px' }}>
-                                {Functions.oneRecordFn(ArrItem.id, ArrItem.vlad, ArrItem.alex)}
+                            :
+                            (<div style={{ minWidth: '300px' }}>
+                                {showOnerecord(ArrItem.id, ArrItem.vlad, ArrItem.alex)}
                             </div>)
                     }
                 </div>
