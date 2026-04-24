aba3941c884016ea8661da37179ca0b7b231b627
Alexander <alex.schlieden@gmail.com>
2026-04-24 14:54:50 +0200

    add a new header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,40 +1,14 @@
-/* import { NavLink } from "react-router-dom"; */
+
 import style from './Header.module.css';
-
-
-
 
 const Header = () => {
     return (
         <header>
-            <h1 className={style.nav}>HEADEWR</h1>
-            {/*  <div >
-                <nav className={style.nav}>
-                    <ul>
-                        {Items.map(item =>
-                            <li key={item.title}>
-                                <NavLink
-                                    className={({ isActive }) => isActive ? style.navActive : undefined}
-                                    to={item.to}>
-                                    {item.title}
-                                </NavLink>
-                            </li>)}
-                    </ul>
-
-                    <ul>
-                        {Languages.map(item =>
-                            <li key={item.title}>
-                                <NavLink
-                                    className={({ isActive }) => isActive ? style.navActive : undefined}
-                                    to={item.to}>
-                                    {item.title}
-                                </NavLink>
-                            </li>)}
-                    </ul>
-
-                </nav>
-            </div> */}
-        </header>
+            <div className={style.nav}>
+                <h1 className={style.navHeader}>fish records</h1>
+                <a className={style.navHeaderLink} href="https://a-schlieden.github.io/time2fishing/">Prepare the tackle</a>
+            </div>
+        </header >
     )
 }
 
